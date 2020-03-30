4cf05e88416f70f66b9839e2c05513868875a04b
Dua <duaelias9@gmail.com>
2020-03-29 23:33:45 -0400

    Primeiro Modelo Controlador Visão Departamento

---
+++ SalesWebMvc/Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace SalesWebMvc.Models.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestID => !string.IsNullOrEmpty(RequestId);
+    }
+}
